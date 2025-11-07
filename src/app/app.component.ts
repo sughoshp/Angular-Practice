5d5f4d8861baca4dc21b2c6ba5cb3a6039ad015c
Sughosh Pande <sughoshpandes1@gmail.com>
2025-11-07 18:11:37 +0530

    counter-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,30 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'counter-app';
+  count: number = 0;
+  increment = () => {
+    this.count++;
+  }
+  decrement = () => {
+    this.count--;
+  }
+  reset = () => {
+    this.count = 0;
+  }
+  handleCounter = (val: string) => {
+    if (val == 'minus') {
+      this.count--;
+    } else if (val == 'plus') {
+      this.count++;
+    } else {
+      this.count = 0;
+    }
+
+  }
 }
